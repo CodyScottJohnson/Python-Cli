d8672c109cf993e44f4ec9119f389fdc3234c8ec
Cody Johnson <dev@codyscottjohnson.com>
2019-09-04 08:52:41 -0700

    I don't know

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 setup(
-    name = 'dbx-tableau-scraper',
+    name = 'cli_name',
     version = '0.2.0',
-    install_requires=['pandas','click','numpy','tableaudocumentapi','sqlparse'],
-    packages = ['dbx_tableau_scraper'],
+    install_requires=['click'],
+    packages = ['cli_name'],
     entry_points = {
         'console_scripts': [
-            'tableau-scraper = dbx_tableau_scraper.__main__:main'
+            'cli_name = cli_name.__main__:main'
         ]
     })
